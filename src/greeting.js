814a72438192938a53b3862680cd7e187571203f
Roshini_GS <153312227+23AIML043@users.noreply.github.com>
2025-01-29 09:08:58 +0530

    Delete src directory

--- src/greeting.js
+++
@@ -1,7 +0,0 @@
-//import React from 'react';
-const greetingMessage = ' Hello, from the coutom module!';
-const greet = () => {
-    console.log(greetingMessage);
-};
-module.exports= greet;
-//export default greet;
